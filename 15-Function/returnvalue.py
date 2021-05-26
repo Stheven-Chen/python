5baa1085f6f1584e6c6c415f357d319da982ab2c
Stheven <stheven4496@gmail.com>
2021-05-26 18:11:19 +0700

    add input functioin with def function

--- 15-Function/returnvalue.py
+++ 15-Function/returnvalue.py
@@ -23,3 +23,16 @@ def kali(argument1, argument2):
 
 
 b = kali(3,tambah(3,4))
+
+print(20*'=')
+def smaller_num(x, y): ## Can be rephrased to  def smaller_num(x, y):
+    if x > y:          ##                          if x > y:
+        number = y     ##                              return y
+    else:              ##                          else:
+        number = x     ##                              return x
+    return number
+
+x = input("Enter first number:-")
+y = input("Enter second number:-")
+result = smaller_num(x, y)
+print("The smaller number between " +  str(x) + " and " + str(y) + " is " + str(result))
